215b5d37a538d8b498050038dd06477fa48d9a2b
Page-MS <pagems@proton.me>
2024-10-25 11:38:50 +0200

    Original directory

---
+++ Pieces.cpp
@@ -0,0 +1,5 @@
+//
+// Created by page on 25/10/2024.
+//
+
+#include "Pieces.h"
---
+++ Pieces.h
@@ -0,0 +1,8 @@
+//
+// Created by page on 25/10/2024.
+//
+
+#ifndef LO21_PIECES_H
+#define LO21_PIECES_H
+
+#endif //LO21_PIECES_H
