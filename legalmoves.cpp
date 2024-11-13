766d815f1614a4ad777ff437d76e7717939266d0
Page-MS <pagems@proton.me>
2024-11-13 09:55:34 +0100

    new files

---
+++ legalmoves.cpp
@@ -0,0 +1,5 @@
+//
+// Created by page on 13/11/2024.
+//
+
+#include "legalmoves.h"
---
+++ legalmoves.h
@@ -0,0 +1,8 @@
+//
+// Created by page on 13/11/2024.
+//
+
+#ifndef LO21_LEGALMOVES_H
+#define LO21_LEGALMOVES_H
+
+#endif //LO21_LEGALMOVES_H
