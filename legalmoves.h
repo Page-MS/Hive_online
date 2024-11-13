5d96924a274904efe838a71c64635384d702f3d1
Page-MS <pagems@proton.me>
2024-11-13 12:37:23 +0100

    LegalMoveContext

--- legalmoves.h
+++ legalmoves.h
@@ -4,5 +4,21 @@
 
 #ifndef LO21_LEGALMOVES_H
 #define LO21_LEGALMOVES_H
+#include "pieces.h"
+class LegalMoveContext{
+private:
+    LegalMoveContext();
+    //on supprime l'affectation et la copie
+    LegalMoveContext(const LegalMoveContext&) = delete;
+    LegalMoveContext& operator=(const LegalMoveContext&) = delete;
+public:
+    //usage de singleton pour obtenir l'unique instance du legalMoveContext
+    static LegalMoveContext& getInstance(){
+        LegalMoveContext instance;
+        return instance;
+    }
+    int** searchLegalMoves(TYPE_PIECE typePiece,int x, int y);
+
+};
 
 #endif //LO21_LEGALMOVES_H
--- pieces.h
+++ pieces.h
@@ -17,16 +17,16 @@ Scarabee,Abeille,Fourmi,Sauterelle, Araignee, Moustique, Coccinelle
 class Piece {
     int x_position;
     int y_position;
-    TYPE_PIECE typePiece;
+    TYPE_PIECE type_piece;
     friend class Mouvement;
     friend class Case;
     friend class GameManager;
 public:
     //Si les pièces sont crées avant d'être mises sur le plateau, elles sont alors toutes placées à un emplacement abérant
-    Piece(TYPE_PIECE type,int x=-100, int y=-100): x_position(x), y_position(y), typePiece(type){}
+    Piece(TYPE_PIECE type,int x=-100, int y=-100): x_position(x), y_position(y), type_piece(type){}
     ~Piece()=default;
     int** coupsPossibles();
-    TYPE_PIECE get_type(){return typePiece;}
+    TYPE_PIECE get_type(){return type_piece;}
 
 };
 
