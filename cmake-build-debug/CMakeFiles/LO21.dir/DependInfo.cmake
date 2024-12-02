9e8e266c32c884f238ea6584df75eb1d001853a9
Page-MS <pagems@proton.me>
2024-12-02 19:16:32 +0100

    fix of the gitignore

--- cmake-build-debug/CMakeFiles/LO21.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/LO21.dir/DependInfo.cmake
@@ -18,8 +18,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/page/Documents/UTC/A24/LO21/Projet/LO21/plateau.cpp" "CMakeFiles/LO21.dir/plateau.cpp.obj" "gcc" "CMakeFiles/LO21.dir/plateau.cpp.obj.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
