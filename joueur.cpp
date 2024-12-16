29dcdebdb848684f3b8abc7721df3c63479763d7
Page-MS <pagems@proton.me>
2024-12-16 10:50:05 +0100

    There were more hidden conflicts in joueur and partie

--- joueur.cpp
+++ joueur.cpp
@@ -1,4 +1,26 @@
 #include "joueur.h"
+#include <algorithm>
+
+Joueur::Joueur(const std::string& nomJoueur, bool IA) : nom(nomJoueur), isIA(IA) {
+    map<TYPE_PIECE, int> piecesNecessaires = {
+        {Araignee, 2},
+        {Sauterelle, 3},
+        {Fourmi, 3},
+        {Scarabee, 2},
+        {Moustique, 1},
+        {Coccinelle, 1},
+        {Abeille, 1}
+    };
+
+    for (const auto& typeDePiecce : piecesNecessaires) {
+        TYPE_PIECE modele = typeDePiecce.first;
+        int nombre = typeDePiecce.second;
+
+        for (int i = 0; i < nombre; ++i) {
+            pieces.push_back(new Piece(modele, IA));
+        }
+    }
+}
 
 Joueur& Joueur::operator=(const Joueur& autre) {
     if (this != &autre) { // évite la copie de soi-même
@@ -13,8 +35,57 @@ Joueur& Joueur::operator=(const Joueur& autre) {
     return *this;
 }
 
-void Joueur::jouerCoup(Piece* pieceChoisie, const Coords& destination, Plateau& plateau){
+void Joueur::jouerCoupCreer(Piece* pieceChoisie, const Coords& destination, Plateau& plateau) {
+    if (!plateau.inReserve(*pieceChoisie)) {
+        throw std::invalid_argument("La pièce choisie n'est pas dans la réserve.");
+    }
 
+    if (!plateau.canPlace(*pieceChoisie, destination)) {
+        throw std::invalid_argument("La position cible est invalide ou occupée.");
+    }
+
+    // Obtenir les mouvements légaux de la pièce
+    LegalMoveContext& moveContext = LegalMoveContext::getInstance();
+    moveContext.changeStrategy(pieceChoisie->getType());
+    Graphe copygraphe=plateau.getGraphe();
+    vector<Coords> legalMoves = moveContext.searchLegalMoves(destination, &copygraphe, pieceChoisie->getCamp());
+
+    //parcours les legalmoves pour verifier que destination en fait bien partie
+    auto it = std::find(legalMoves.begin(), legalMoves.end(), destination);
+    if (it == legalMoves.end()) {
+        throw std::invalid_argument("La destination n'est pas un mouvement légal pour cette pièce.");
+    }
+
+    plateau.movePiece(*pieceChoisie, destination);
+}
+
+void Joueur::jouerCoupDeplacer(Piece* pieceChoisie, const Coords& destination, Plateau& plateau){
+    if (plateau.inReserve(*pieceChoisie)) {
+        throw std::invalid_argument("La pièce choisie est dans la réserve.");
+    }
+
+    if (plateau.isPieceStuck(*pieceChoisie)){
+        throw std::invalid_argument("La pièce choisie est bloqué sous une autre pièce.");
+    }
+
+    if (!plateau.canPlace(*pieceChoisie, destination)) {
+        throw std::invalid_argument("La position cible est invalide ou occupée.");
+    }
+
+
+    // Obtenir les mouvements légaux de la pièce
+    LegalMoveContext& moveContext = LegalMoveContext::getInstance();
+    moveContext.changeStrategy(pieceChoisie->getType());
+    Graphe copygraphe=plateau.getGraphe();
+    vector<Coords> legalMoves = moveContext.searchLegalMoves(destination, &copygraphe, pieceChoisie->getCamp());
+
+    //parcours les legalmoves pour verifier que destination en fait bien partie
+    auto it = std::find(legalMoves.begin(), legalMoves.end(), destination);
+    if (it == legalMoves.end()) {
+        throw std::invalid_argument("La destination n'est pas un mouvement légal pour cette pièce.");
+    }
+
+    plateau.movePiece(*pieceChoisie, destination);
 }
 
 
--- joueur.h
+++ joueur.h
@@ -4,12 +4,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include "plateau.h"
+#include "legalmoves.h"
+
+
 #include <initializer_list>
 #include <array>
 #include <cstdlib>
 
 using namespace std;
+class EtatDuJeu;
 
 class Joueur {
 protected:
@@ -18,21 +23,26 @@ protected:
     bool isIA;
 
 public:
-    Joueur(const std::string& nomJoueur, bool IA=false) : nom(nomJoueur), isIA(IA) {}
+    Joueur(const std::string& nomJoueur, bool IA=false);
     Joueur(){}
     Joueur(const Joueur& autre) : nom(autre.nom) {
         for (Piece* piece : autre.pieces) {
             pieces.push_back(new Piece(*piece)); // copie chaque pièce
         }
     }
-    Joueur& operator=(const Joueur& autre);*
+    Joueur& operator=(const Joueur& autre);
     ~Joueur() {
         for (Piece* piece : pieces) {
             delete piece; // libère chaque pièce
         }
     }
 
-    void jouerCoup(Piece* pieceChoisie, const Coords& destination, Plateau& plateau); // joueur joue un coup
+    void jouerCoupCreer(Piece* pieceChoisie, const Coords& destination, Plateau& plateau);
+
+    void jouerCoupDeplacer(Piece* pieceChoisie, const Coords& destination, Plateau& plateau);
+
+
+
 
     string getNom() const { return nom; }
 
@@ -40,7 +50,7 @@ public:
 
     bool getIsIA() { return isIA; }
 
-    vector<Piece*> getPieces(){return pieces;}
+    vector<Piece*> getPieces() {return pieces;}
 };
 
 
@@ -60,34 +70,6 @@ public:
     void ExecuterMvt();  // exécute le déplacement
     void AnnulerMvt();   // annule le déplacement
 };
-
-
-
-class EtatDuJeu {
-private:
-    Plateau plateau;               // plateau avec les pièces
-    vector<Mouvement> historique;  // liste des coups
-    Joueur* joueurs[2];              // liste des joueurs
-    int numero_tour;
-    Joueur* joueur_courant;
-
-public:
-    EtatDuJeu(int num_tour, const Plateau& p,  const Joueur& j1, const Joueur& j2, const Joueur& jc);
-    EtatDuJeu();
-    EtatDuJeu& operator=(const EtatDuJeu& jeu);
-    vector<Mouvement> coupPossibles(Joueur& j);  // coups possibles
-    bool FinDuJeu() const;  // vérifie si le jeu est fini
-    void ajouterMouvement(const Mouvement& mvt) { historique.push_back(mvt); } // ajoute un coup à l'historique
-    void annulerDernierMouvement(); // annule le dernier coup
-    Plateau& getPlateau() { return plateau; } // accès au plateau
-
-    Joueur* getJoueurCourant() {return joueur_courant;}
-
-};
-
-
-
-
 
 class IAJoueur : public Joueur {
 private:
